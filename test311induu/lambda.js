4e32ffd6226aad302d916cb69258d91ae9fbf5f0
Indunil1992 <indunil@adroitlogic.com>
2019-12-18 14:39:00 +0530

    2

--- test311induu/lambda.js
+++ test311induu/lambda.js
@@ -14,12 +14,12 @@ exports.handler = async (event) => {
             data = {
                 Contents: [
                     {
-                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                       ETag: "\"70 cee1738b6b21e2c8a43f3a5ab0eee71\"",
                        Key: "example1.jpg",
                        LastModified: "<Date Representation>",
                        Owner: {
                           DisplayName: "myname",
-                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                          ID: "12345example25102679df2c7bb0ae12b3f85be6f290b936c4393484be31bebcc"
                        },
                        Size: 11,
                        StorageClass: "STANDARD"
